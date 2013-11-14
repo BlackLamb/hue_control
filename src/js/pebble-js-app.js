026184a9e6eb1574e8c1fda4f37e5930f208b587
BlackLamb <blamb001@gmail.com>
2013-11-13 23:15:35 -0800

    Trying java api

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,5 +1,17 @@
 Pebble.addEventListener("ready",
     function(e) {
         console.log("Hello world! - Sent from your javascript application.");
+		
+		var transactionId = Pebble.sendAppMessage({'0': 42, '1': 'string value'},
+												  function(e) {
+												  	  console.log("Successfully delivered message with transactionId="
+															 + e.data.transactionId);
+												  },
+												  function(e) {
+													  console.log("Unable to deliver message with transactionID="
+																 + e.data.transactionId
+																 + " Error is: " + e.error.message);
+												  }
+	   );
     }
 );
